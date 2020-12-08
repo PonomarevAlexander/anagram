77e99b342f7bef25490cb2b83079506335594a81
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2020-12-09 00:34:49 +0300

    add StringModifer class

---
+++ src/com/ponomarev/main/StringModifer.java
@@ -0,0 +1,50 @@
+package com.ponomarev.main;
+import java.util.StringJoiner;
+
+
+public class StringModifer {
+    
+    public String reverseString (String data) {
+        final String SPACE = " ";
+        StringJoiner joiner = new StringJoiner(SPACE);
+        String[] splittedSentence = data.split(SPACE);
+        String[] result = new String[splittedSentence.length];
+        
+        for (int i = 0; i < splittedSentence.length; i++) {
+            char[] word = getWordByIndex(splittedSentence, i);
+            joiner.add(toReverseWordIgnoreNonAlphabetic(word));
+        }
+        return joiner.toString();
+    }
+    
+    
+    private char[] getWordByIndex(String[] sentenceArray, int index) {
+        return sentenceArray[index].toCharArray();
+    }
+    
+    
+    private char[] swapSymbols(char[] symbols, int leftIndex, int rightIndex) {
+        char temporaryIndex = symbols[leftIndex];
+        symbols[leftIndex] = symbols[rightIndex];
+        symbols[rightIndex] = temporaryIndex;
+        return symbols;
+    }
+    
+    private String toReverseWordIgnoreNonAlphabetic(char[] word) {
+       int rightIndex = word.length - 1, leftIndex = 0;
+       while (leftIndex < rightIndex) {
+           if (!Character.isAlphabetic(word[leftIndex])) {
+               leftIndex++;
+           }
+           else if (!Character.isAlphabetic(word[rightIndex])) {
+               rightIndex--;
+           }
+           else {
+               word = swapSymbols(word, leftIndex, rightIndex);
+               leftIndex++;
+               rightIndex--;
+           }
+       }
+       return new String(word);
+    }
+}
