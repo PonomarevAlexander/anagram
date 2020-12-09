4cf7e24755dc24ef9ce8d9d1232c6324407df5b6
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2020-12-09 23:26:55 +0300

    SPACE constant changed modifiers private and static

--- src/com/ponomarev/test/StringModiferTest.java
+++ src/com/ponomarev/test/StringModiferTest.java
@@ -3,27 +3,53 @@ package com.ponomarev.test;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import com.ponomarev.main.*;
 
 public class StringModiferTest {
 
     StringModifer sm;
-
+    private static final String ONLY_LETTER = "qwerty asdfgh zxcvbn";
+    private static final String ONLY_LETTER_REVERSED ="ytrewq hgfdsa nbvcxz";
+    private static final String ONLY_DIGITS = "1234 567 89 0";
+    private static final String ONLY_DIGITS_REVERSED = "1234 567 89 0";
+    private static final String ONLY_SPEC_SYMBOLS = "!@#$ %^& *( )";
+    private static final String ONLY_SPEC_SYMBOLS_REVERSED = "!@#$ %^& *( )";
+    private static final String LETTERS_AND_SPEC_SYMBOLS = "q!w@er#ty$ %asd^fg&h z*(xcvb{n";
+    private static final String LETTERS_AND_SPEC_SYMBOLS_REVERSED = "y!t@re#wq$ %hgf^ds&a n*(bvcx{z";
+    private static final String LETTERS_AND_DIGITS = "q1we2rt3y 4as5df6gh 7zxc8vb90n";
+    private static final String LETTERS_AND_DIGITS_REVERSED = "y1tr2ew3q 4hg5fd6sa 7nbv8cx90z";
+    private static final String DIGITS_AND_SPEC_SYMBOLS = "12!@34# 56%^7 ^8&9 *0";
+    private static final String DIGITS_AND_SPEC_SYMBOLS_REVERSED = "12!@34# 56%^7 ^8&9 *0";
+    private static final String LETTERS_AND_DIGITS_AND_SPEC_SYMBOLS = "#q1w@e2rt%^^&3y 4as5%*df6&*gh 7z*(xc8vb9**0n";
+    private static final String LETTERS_AND_DIGITS_AND_SPEC_SYMBOLS_REVERSED = "#y1t@r2ew%^^&3q 4hg5%*fd6&*sa 7n*(bv8cx9**0z";
+    private static final String EMPTY_STRING = "";
+    private static final String EMPTY_STRING_REVERSED = "";
+    private static final String NULL_EXEPTION_MESSEGE = "Wrong argument, NULL aren't allowed!!!";
+    
     @Before
     public void initObject() {
         sm = new StringModifer();
     }
     
-    @Test(expected = NullPointerException.class)
+    @Test
     public void toReverseStringConsistingOfNullTest() {
-        String source = sm.reverseString(null);
+        try {
+            String source = sm.reverseString(null);
+            fail("Expected exception wasn't thrown");
+        }
+        catch (IllegalArgumentException ex){
+            assertEquals(NULL_EXEPTION_MESSEGE, ex.getMessage());
+        } 
     }
     
     @Test
     public void toReverseStringConsistingOfEmptyStringTest() {
-        String source = "";
-        String expected = "";
+        String source = EMPTY_STRING;
+        String expected = EMPTY_STRING_REVERSED;
         
         String actual = sm.reverseString(source);
         assertEquals(expected, actual);
@@ -31,8 +57,8 @@ public class StringModiferTest {
     
     @Test
     public void toReverseStringConsistingOfLettersOnlyTest() {
-        String source = "qwerty asdfgh zxcvbn";
-        String expected = "ytrewq hgfdsa nbvcxz";
+        String source = ONLY_LETTER;
+        String expected = ONLY_LETTER_REVERSED;
         
         String actual = sm.reverseString(source);
         assertEquals(expected, actual);
@@ -40,8 +66,8 @@ public class StringModiferTest {
     
     @Test
     public void toReverseStringConsistingOfDigitsOnlyTest() {
-        String source = "1234 567 89 0";
-        String expected = "1234 567 89 0";
+        String source = ONLY_DIGITS;
+        String expected = ONLY_DIGITS_REVERSED;
         
         String actual = sm.reverseString(source);
         assertEquals(expected, actual);
@@ -49,8 +75,8 @@ public class StringModiferTest {
     
     @Test
     public void toReverseStringConsistingOfSpecSymbolsOnlyTest() {
-        String source = "!@#$ %^& *( )";
-        String expected = "!@#$ %^& *( )";
+        String source = ONLY_SPEC_SYMBOLS;
+        String expected = ONLY_SPEC_SYMBOLS_REVERSED;
         
         String actual = sm.reverseString(source);
         assertEquals(expected, actual);
@@ -58,8 +84,8 @@ public class StringModiferTest {
     
     @Test
     public void toReverseStringConsistingOfLettersAndDigitsTest() {
-        String source = "q1we2rt3y 4as5df6gh 7zxc8vb90n";
-        String expected = "y1tr2ew3q 4hg5fd6sa 7nbv8cx90z";
+        String source = LETTERS_AND_DIGITS;
+        String expected = LETTERS_AND_DIGITS_REVERSED;
         
         String actual = sm.reverseString(source);
         assertEquals(expected, actual);
@@ -67,8 +93,8 @@ public class StringModiferTest {
     
     @Test
     public void toReverseStringConsistingOfLettersAndSpecSymbolsTest() {
-        String source = "q!w@er#ty$ %asd^fg&h z*(xcvb{n";
-        String expected = "y!t@re#wq$ %hgf^ds&a n*(bvcx{z";
+        String source = LETTERS_AND_SPEC_SYMBOLS;
+        String expected = LETTERS_AND_SPEC_SYMBOLS_REVERSED;
         
         String actual = sm.reverseString(source);
         assertEquals(expected, actual);
@@ -76,8 +102,8 @@ public class StringModiferTest {
     
     @Test
     public void toReverseStringConsistingOfDigitsAndSpecSymbolsTest() {
-        String source = "12!@34# 56%^7 ^8&9 *0";
-        String expected = "12!@34# 56%^7 ^8&9 *0";
+        String source = DIGITS_AND_SPEC_SYMBOLS;
+        String expected = DIGITS_AND_SPEC_SYMBOLS_REVERSED;
         
         String actual = sm.reverseString(source);
         assertEquals(expected, actual);
@@ -85,8 +111,8 @@ public class StringModiferTest {
     
     @Test
     public void toReverseStringConsistingLettersAndDigitsAndSpecSymbolsTest() {
-        String source = "#q1w@e2rt%^^&3y 4as5%*df6&*gh 7z*(xc8vb9**0n";
-        String expected = "#y1t@r2ew%^^&3q 4hg5%*fd6&*sa 7n*(bv8cx9**0z";
+        String source = LETTERS_AND_DIGITS_AND_SPEC_SYMBOLS;
+        String expected = LETTERS_AND_DIGITS_AND_SPEC_SYMBOLS_REVERSED;
         
         String actual = sm.reverseString(source);
         assertEquals(expected, actual);
