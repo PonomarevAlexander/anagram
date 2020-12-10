9efc35f09e978a68e03c96d8320cd9b1edc012b7
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2020-12-10 10:49:44 +0300

    add exception

--- src/com/ponomarev/main/Main.java
+++ src/com/ponomarev/main/Main.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException{
         Scanner scanner = new Scanner(System.in);
         StringModifer stringModifer = new StringModifer();
         
         while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("q")) {
-                scanner.close();
-                break;
-            }  
-            else {
-                System.out.println(stringModifer.reverseString(input));
-                System.out.println();
-            }      
+            try {
+                String input = scanner.nextLine();
+                if (input.equals("q")) {
+                    scanner.close();
+                    break;
+                } 
+                else {
+                    System.out.println(stringModifer.reverseString(input));
+                    System.out.println();
+                }
+            } catch (IllegalArgumentException ex) {
+                System.out.println("Wrong argument, NULL aren't allowed!!!");
+            }
         }
     }
 }
